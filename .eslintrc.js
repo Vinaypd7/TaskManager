f0b134ea3f50de327743ba99848c759ff25ec433
Vinayaka Duggappa <Vinayaka.Duggappa@evryindia.in>
2025-11-19 12:24:43 +0530

    chore: add ESLint and basic config

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended'
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // project-specific rule overrides can go here
+  },
+};
