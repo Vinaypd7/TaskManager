c55988cf0284bbc579e84c97ba77e6afe01c1d34
Vinayaka P D <vinaypd37@gmail.com>
2026-01-06 13:26:03 +0530

    feat(auth): address PR comments, add StorageKeysEnum, use ROLES enum, lock package versions

---
+++ src/constants/storageKeys.ts
@@ -0,0 +1,8 @@
+/**
+ * Storage Keys Enum
+ * Centralized enum for all AsyncStorage and local storage keys
+ * Provides better type safety and maintainability
+ */
+export enum StorageKeysEnum {
+  USER_SESSION = 'user_session',
+}
